95a44636f99b61e144ef9e26f016aac68f6c6fe5
liu <1005674205@qq.com>
2020-09-20 16:11:36 +0800

    network

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,19 +1,33 @@
 <template>
   <div id="home">
-    <nav-bar>
-      
-    </nav-bar>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
   </div>
 </template>
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultidata} from 'network/home'
 export default {
   name:'Home',
   components:{
     NavBar
-  }
+  },
+  data() {
+    return {
+      
+    }
+  },
+  created() {
+    // 1.亲求多个数据
+    getHomeMultidata().then( res => {
+      console.log(res);
+      
+    })
+  },
 }
 </script>
 <style scoped>
-
+.home-nav{
+  background: #ff5777;
+  color: #fff;
+}
 </style>
